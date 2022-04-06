5a62751e96528a12f2711b4c9aba5dce76f04713
Park SungHwan <park0219ok@outlook.com>
2022-04-07 00:15:48 +0900

    feat: 목록 화면(홈) 완료

--- src/App.js
+++ src/App.js
@@ -33,8 +33,41 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
+const dummyData = [
+    {
+        id: 1,
+        emotion: 1,
+        content: "오늘의 일기 1번",
+        date: 1649253393410,
+    },
+    {
+        id: 2,
+        emotion: 2,
+        content: "오늘의 일기 2번",
+        date: 1649253393411,
+    },
+    {
+        id: 3,
+        emotion: 3,
+        content: "오늘의 일기 3번",
+        date: 1649253393412,
+    },
+    {
+        id: 4,
+        emotion: 4,
+        content: "오늘의 일기 4번",
+        date: 1649253393413,
+    },
+    {
+        id: 5,
+        emotion: 5,
+        content: "오늘의 일기 5번",
+        date: 1649253393414,
+    },
+];
+
 function App() {
-    const [data, dispatch] = useReducer(reducer, []);
+    const [data, dispatch] = useReducer(reducer, dummyData);
 
     const dataId = useRef(0);
 
